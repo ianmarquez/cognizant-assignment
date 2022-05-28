5fa5ae13c91d09d61756a33cddb534e3509f7922
Ian Marquez <regilmarquez123@gmail.com>
2022-05-28 16:36:26 +0800

    Initial front end layout

---
+++ client/src/components/Sidebar.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Menu, Avatar } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+import '../styles/Sidebar.css';
+
+const menuItems = [
+  { label: 'Function 1', key: 'function-1' },
+  { label: 'Function 2', key: 'function-2' },
+  { label: 'Function 3', key: 'function-3' },
+  { label: 'Function 4', key: 'function-4' },
+  { label: 'Function 5', key: 'function-5' }
+]
+
+const sidebar: React.FC = () => (
+  <>
+    <Avatar size={150} style={{ marginTop: '30px' }} icon={<UserOutlined/>}/>
+    <div className="username"> Long User Name </div>
+    <Menu
+      style={{ marginTop: '30px' }}
+      items={menuItems}
+      theme='dark'
+      mode='inline'
+    />
+  </>
+)
+
+
+export default sidebar;
